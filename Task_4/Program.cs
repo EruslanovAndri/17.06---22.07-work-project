13bb95bdf1290b3475950f3c9055760ac4735035
EruslanovAndrei <eruslanov84as@gmail.com>
2022-06-22 01:04:01 +0300

    add task_4 new version

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -15,34 +15,14 @@ Console.WriteLine("Введите второе число");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число");
 int c = Convert.ToInt32(Console.ReadLine());
-int max = (a);
-
-
-if (a > max);
-else 
-{
-    Console.WriteLine (max = (a));
-}
-if (max > b);
-else 
-{
-     Console.WriteLine (max = (b));
-}
-
-if (max > c);
-
-else 
-
-{
-    
-    Console.WriteLine(max = (c));
-    
-}
-
-// Не могу понять как в конце программы вывести на экран max = (число).
+int max = a;
 
 
 
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
 
-
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
 
