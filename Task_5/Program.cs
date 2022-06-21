b7ed354298e50cc28a70052c9ebd058298892819
EruslanovAndrei <eruslanov84as@gmail.com>
2022-06-21 19:50:04 +0300

    add Task_4 and task_5

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
 // является ли число чётным (делится ли оно на два без остатка)
 
-int num = 6;
-int divider = 2;
-int result = num / divider;
-Console.Write(result);
+Console.WriteLine ("Введите целое число");
+//Console.ReadLine();
+int num = Convert.ToInt32(Console.ReadLine());
