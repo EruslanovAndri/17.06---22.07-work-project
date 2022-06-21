69fa38ef5614151951382d7a56612a5f13abf0ac
EruslanovAndrei <eruslanov84as@gmail.com>
2022-06-21 16:38:45 +0300

    add task_5

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -5,22 +5,8 @@
 // -3 -> нет
 // 7 -> нет
 
-Console.WriteLine("Программа определяющая чётные числа");
-Console.WriteLine("Введите число");
-int num = Convert.ToInt32(Console.ReadLine());
 
-if
-
- (even_number = num / 2);
-Console.WriteLine("The number is even_number");
-esle 
-
-// if (num / even_number)
-// // else 
-// // {
-// //     Console.WriteLine("The number is even_number");
-// // }
-    
-
-
-
+int num = 6;
+int divider = 2;
+int result = num / divider
+Console.Write(result);
