3b1e7a21ffd0fed654bef53a8daca010bb82ad03
EruslanovAndrei <eruslanov84as@gmail.com>
2022-06-24 19:36:07 +0300

    Task_11 Нахождение среднего числа из трёхзначного числа

---
+++ Task_11/Program.cs
@@ -0,0 +1,21 @@
+﻿// // Задача № 10 Напишите программу,которая принимает 
+// //на вход трёхзначное число и на выходе показывает вторую чифру числа.
+
+int rndNum = new Random().Next(100,1000);
+Console.WriteLine($"Случайное трёхзначное число из диапазона от 100  до 999: {rndNum}");
+
+int ShowSecDigit(int num)
+{
+    int firstNumber = num / 100;
+    int secondNumber = num / 10;
+    int thirdDigit = num % 10;
+    int result = secondNumber % 10;
+    return result;
+}
+int res = ShowSecDigit(rndNum);
+Console.WriteLine($"Среднее число из трёхзначного = {res}"); // Ура!!! У меня получилось)
+
+
+
+
+
