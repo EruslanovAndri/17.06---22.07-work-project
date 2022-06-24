7144952dafd15b37b3253d44953c666f107952c5
EruslanovAndrei <eruslanov84as@gmail.com>
2022-06-24 11:17:55 +0300

    Task_8 методы и тернарные выражения

---
+++ Task_8/Program.cs
@@ -0,0 +1,43 @@
+﻿// 9. Напишите программу , которая выводит 
+// случайное число из отрезка (10,99) и показывает 
+// наибольшую цифру числа.
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+int rndNum = new Random().Next(10, 100);
+// Console.WriteLine($"Случайное число из отрезка 10 - 99 => {rndNum}");
+// int firstDigit = rndNum / 10;
+// int secondDigit = rndNum % 10;
+// Console.Write($"Наибольшая цифра  числа {rndNum} => ");
+
+// if (firstDigit > secondDigit) Console.WriteLine(firstDigit);
+// else Console.WriteLine(secondDigit);
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine(result);
+//Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit); // Тернарный оператор.
+// void function(string str)
+// {
+//     Console.WriteLine(str);
+// }
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit == secondDigit) return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+ int maxDigit = MaxDigit(rndNum);
+ string result = maxDigit > 0 ? maxDigit.ToString() : "цифры одинаковые";
+ Console.WriteLine($"Наибольшая цифра числа {rndNum} => {result}");
+
+// string s = "Hello!";
+//  function(s);
+
+
+// int rndNum3 = new Random();
+// while (true)
+// {
+//     int rnd = rndNum3.Next();
+// }
