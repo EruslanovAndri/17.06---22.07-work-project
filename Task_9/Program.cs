bec00b178860bec4923b776150ab261b9b99e7d3
EruslanovAndrei <eruslanov84as@gmail.com>
2022-06-24 11:55:53 +0300

    task_9 удаление среднего числа из трехзначного числа

---
+++ Task_9/Program.cs
@@ -0,0 +1,28 @@
+﻿// // 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+// int rndNum = new Random().Next (100,500);
+// Console.WriteLine($"Случайное трех значное число {rndNum}");
+// int firstDigit = rndNum / 100;
+// int secondDigit = rndNum / 10;
+// int thirdDigit = rndNum % 10;
+
+// Console.Write(firstDigit);
+// Console.Write(thirdDigit);
+
+int rndNum = new Random().Next(100,1000);
+Console.WriteLine($"Случайное трех значное число {rndNum}");
+int RemSecDigit(int num)
+{
+int firstDigit = num / 100;
+int secondDigit = num % 10;
+int result = firstDigit * 10 + secondDigit;
+return result;
+}
+int res = RemSecDigit(rndNum);
+Console.WriteLine(res);
