65406069a64abf937a19df010424858eed405188
EruslanovAndrei <eruslanov84as@gmail.com>
2022-06-28 16:19:59 +0300

    Task_18 нахождение по номеру четверти координаты x и y

---
+++ Task_18/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+int num = Convert.ToInt32(Console.ReadLine());
+string result;
+string Quard(int a)
+
+{
+    string res = "";
+    if (a == 1) res = "x>0 и y >0";
+    if (a == 2) res = "x<0 и y >0";
+    if (a == 3) res = "x<0 и y <0";
+    if (a == 4) res = "x>0 и y <0";
+    return res;
+
+}
+
+if (num > 0 && num < 5)
+{
+    result = Quard(num);
+}
+else
+{
+    result = "Некорректный ввод";
+}
+
+Console.Write(result);
