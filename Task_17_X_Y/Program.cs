759d66d8e41a52627e1bdc687f211f6009268958
EruslanovAndrei <eruslanov84as@gmail.com>
2022-06-28 15:41:22 +0300

    Task_17 Нахождение области по координатам X и Y

---
+++ Task_17_X_Y/Program.cs
@@ -0,0 +1,42 @@
+﻿ //17 Напишите программу, которая  на вход координаты точек ( X Y),
+ //причем X не равен 0 и Y не равен 0 и выдает номер четверти прлоскости , 
+//в которой находится эта точка. 
+
+
+Console.WriteLine("Введите координаты точки X и Y");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+// 1 . Обычный путь решения!!!
+//  if (x > 0 && y > 0)
+//  {
+//     Console.WriteLine("Первая четветрть");
+// }
+// else if (x < 0 && y > 0)
+// {
+//     Console.WriteLine("Вторая четверть");
+// }
+// else if (x < 0 && y < 0)
+// {
+//     Console.WriteLine("Третья четверть");
+// }
+// else if (x > 0 && y < 0)
+// {
+//     Console.WriteLine("Четвертая четверть");
+// }
+// else Console.WriteLine("Введены некорректные координаты");
+
+// 2. Путь решения через метод.
+
+string Quard(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return "Первая четверть";
+    if (xc < 0 && yc > 0) return "Вторая четверть";
+    if (xc < 0 && yc < 0) return "Третья четверть";
+    if (xc > 0 && yc < 0) return "Четвертая четверть";
+    return "Введены некорректные координаты";
+}
+string result = Quard(x, y);
+Console.WriteLine(result);
