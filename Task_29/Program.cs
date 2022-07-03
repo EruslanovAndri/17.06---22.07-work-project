f75ca7bdee8245259e6ccd01b6e6e0fcd172b01e
EruslanovAndrei <eruslanov84as@gmail.com>
2022-07-03 18:15:02 +0300

    add Task 29 Задача 29: Напишите программу, которая задает массив из 8 элементов и выводит их на экран.

---
+++ Task_29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29: Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
+
+int [] array = {1,2,3,4,5,6,7,8,};
+
+void PrintArray(int []arr)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{arr[i]}");
+    }
+    Console.WriteLine();
+}
+PrintArray(array);
