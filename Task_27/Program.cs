ca506d1d4c3c679567e68e5742e86274d5ccef7a
EruslanovAndrei <eruslanov84as@gmail.com>
2022-07-03 17:40:32 +0300

    add Задача 25: Написать программу , которая принимает на вход число и выдает сумму цифр в цисле

---
+++ Task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25: Написать программу , 
+// которая принимает на вход число и выдает сумму цифр в цисле.
+
+Console.WriteLine("Введите любое целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+int count = 0;
+
+while (number > 0)
+{
+    
+    res = res + number % 10;
+    number = number / 10;
+    count++;
+}
+
+Console.WriteLine($"{res}");
+
+
+
+
