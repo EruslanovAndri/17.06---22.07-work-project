5e23921989eb52c7ef55d2c6c9321e44074d7ca9
EruslanovAndrei <eruslanov84as@gmail.com>
2022-07-03 11:28:35 +0300

    add Task 25 возведение числа А в степень числа В

---
+++ Task_25/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 25:  Написать программу, которая принимает на вход два 
+//числа (А и В) и выводит число А в натуральную степень В.
+
+Console.Write("Введите первое число : ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число : ");
+int num2 = int.Parse(Console.ReadLine());
+int res = 1;
+
+for (int i = 0; i < num2; i++)
+{
+    res *= num1;
+}
+Console.WriteLine($"Результат возведение числа {num1} в степень {num2} = {res}");
+
