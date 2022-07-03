0ed637a6028df53ed10acdd419aaa6357385ae4b
EruslanovAndrei <eruslanov84as@gmail.com>
2022-07-03 18:30:38 +0300

    add Task 23 программа ищет куб числа

--- Task_23/Program.cs
+++ Task_23/Program.cs
@@ -3,17 +3,6 @@
 // 3 -> 1,8,27
 // 5 -> 1,8,27,64,125
 
-
-
-// Console.WriteLine(" Введите число ");
-// int n = int.Parse(Console.ReadLine());
-
-// for (int i = 1; i <= n; i++)
-// {
-//     Console.WriteLine($" | Квадрат числа | {i} | = | {i * i, 2} | ");
-// }
-
-
 Console.WriteLine("Введите число ");
 int n = int.Parse(Console.ReadLine());
 
