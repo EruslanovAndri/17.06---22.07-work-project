03dd6d370d6b3a5b2804b36e3ea0abe9a46b3395
EruslanovAndrei <eruslanov84as@gmail.com>
2022-07-05 11:44:40 +0300

    add Task32 замена - на + в массиве

---
+++ Task_32/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] array = {-4,-8,8,2};
+int index = 0;
+
+while (index < array.Length)
+{
+    array[index] = array[index] * -1;
+    index++;
+}
+void PrintArray(int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+{
+    if (i == 0) Console.Write("[");
+    if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+    else Console.Write(arr[i] + "]");    
+}
+}
+PrintArray(array);
+
