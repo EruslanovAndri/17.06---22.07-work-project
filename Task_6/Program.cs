bbae6d5c7e2f2783839e7bcab9d209113dc277f6
EruslanovAndrei <eruslanov84as@gmail.com>
2022-07-05 12:41:49 +0300

    add

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -7,3 +7,15 @@
 // 8 -> 2, 4, 6, 8
 
 Console.WriteLine("Напишите любое число.");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" ");
+int count = 0;
+int res = 0;
+int div = 2;
+
+while( num > 0)
+{
+    num = num / div;
+    count++;
+}
+
