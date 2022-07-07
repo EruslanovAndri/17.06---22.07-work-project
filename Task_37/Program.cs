698f79a1e8efffbff1bf5152f3c726daddd659dd
EruslanovAndrei <eruslanov84as@gmail.com>
2022-07-07 21:46:58 +0300

    Task37 have to be solved

--- Task_37/Program.cs
+++ Task_37/Program.cs
@@ -5,10 +5,10 @@
 // [6 7 3 6] -> 36 21
 
 
-int[] array = { 1, 2, 3, 4, 5 };
-int[] GetProizved(int [] arr)
+int[] array = { 2, 1, 2, 3, 5, 6, };
+int[] ProizvedSideNUmbers(int[] arr)
 {
-    if (arr.Length % 2)
+    if(arr.Length %2) 
     int[] newArr = new int[];
     for (int i = 0; i < arr.Length / 2; i++)
     {
@@ -16,14 +16,18 @@ int[] GetProizved(int [] arr)
     }
     return newArr;
 }
-int[] newArray = GetProizved(array);
+
+int[] newArray = ProizvedSideNUmbers(array);
+
 void PrintArray(int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
     {
         if (i == 0) Console.Write("[");
-        if (i < arr.Length - 1) Console.WriteLine(arr[i] + "+");
-        else Console.WriteLine(arr[i]);
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i] + "]");
     }
 }
 PrintArray(newArray);
+
+// Do not work!!!
