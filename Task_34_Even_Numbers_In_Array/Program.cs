fd8934dcc9f96a6067cf9a5d86ed66bdc6609f02
EruslanovAndrei <eruslanov84as@gmail.com>
2022-07-10 19:47:07 +0300

    Task 34 Поиск количество четных чисел в массиве

---
+++ Task_34_Even_Numbers_In_Array/Program.cs
@@ -0,0 +1,52 @@
+﻿// Task 34. Здайте массив заполненый случайными трехзначнеными числами.
+// Напишите программу,которая покажет количество четных чисел.
+
+int[] CreateArrayRndInt(int size)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+    int index = 0;
+
+    while (index < array.Length)
+    {
+        array[index] = rnd.Next(100, 1000);
+        index++;
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i] + "]");
+    }
+}
+
+int[] SumOfEvenNumbersInArray(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count = count + 1;
+    }
+    return new[] { count };
+}
+
+int[] array = CreateArrayRndInt(10);
+PrintArray(array);
+int[] res = SumOfEvenNumbersInArray(array);
+Console.WriteLine();
+PrintArray(res);
+
+//Work
+
+// Не могу понять как вывести сообщение в консоль о том, что результат равен = res. Табуляция не помогает.
+
+
+
+
