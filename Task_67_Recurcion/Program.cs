0a2b79321ae9745853ae1c250e6d97e91bff9611
EruslanovAndrei <eruslanov84as@gmail.com>
2022-07-19 12:22:21 +0300

    Task67 Рекурсия

---
+++ Task_67_Recurcion/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int finalResult = SumOfTheDidgit(num);
+Console.WriteLine(finalResult);
+
+int SumOfTheDidgit(int didgit)
+{
+    if (didgit > 0) 
+    {
+        return  didgit % 10 + SumOfTheDidgit(didgit / 10);  
+    }
+    return 0;
+}
